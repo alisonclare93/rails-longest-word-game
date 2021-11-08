92510497df721fef2512b21982b9cff5d44301ff
Alison Clare <alisonclare93@gmail.com>
2021-11-08 16:12:59 +0000

    created longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    word = params["word"]
+    letters_array = params["letters_array"].split
+
+    if grid_include?(word, letters_array) == false
+      @message = "Sorry, but #{word} can't be built out of #{letters_array.join}"
+    elsif english_word?(word) == false
+      @message = "Sorry, but #{word} does not seem to be a valid English word..."
+    else
+      @message = "Congratulations! #{word.capitalize} is a valid English word!"
+    end
+  end
+
+  private
+
+  def grid_include?(word, letters_array)
+    word.chars.all? { |letter| word.count(letter) <= letters_array.count(letter.upcase) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+
+end
